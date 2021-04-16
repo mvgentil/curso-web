2123526ba96069300c63b1d20af56daeff766593
Matheus Gentil <matheusvgentil@gmail.com>
2021-04-15 22:09:12 -0300

    adicionando exercicios templateStrings e booleans

---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,15 @@
+const nome = 'Matheus'
+const concatenacao = 'Olá ' + nome + '!'
+
+console.log(concatenacao)
+
+
+const template = `Olá ${nome}!`
+
+console.log(template)
+
+//expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
