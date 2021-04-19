92345e1882a5e49a3c6138431a44262159232d0e
Matheus Gentil <matheusvgentil@gmail.com>
2021-04-19 15:44:30 -0300

    adicionando exercicios de var e let

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,13 @@
+{{{{var sera = 'Será??'}}}}
+
+
+console.log(sera)
+
+{let teste = 'sim'
+console.log(teste)}
+//console.log(teste)
+
+//let = variavel de escopo local
+//var = variavel de escopo global
+
+
---
+++ exercicios-js/fundamentos/usandoVar2.js
@@ -0,0 +1,15 @@
+var numero = 1
+{
+  var numero = 2
+  console.log('dentro =', numero)
+}
+
+console.log('fora =', numero)
+
+/*let teste = 1
+{
+  let teste = 2
+  console.log('dentro =', teste)
+}
+
+console.log('fora =', teste)*/
